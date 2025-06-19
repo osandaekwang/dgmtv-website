bca239a75ee647b461556a74a092492e795c085f
osandaekwang <derek1817@naver.com>
2025-06-19 17:13:43 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,23 @@
-const CACHE_NAME = 'odk-dgm-cache-v1';
-// 캐싱할 파일 목록
+const CACHE_NAME = 'odk-dgm-cache-v2'; // 캐시 버전 업데이트
 const URLS_TO_CACHE = [
   '/',
-  '/index.html',
-  'https://cdn.tailwindcss.com/3.4.16',
-  'https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css'
+  '/index.html'
 ];
 
-// 서비스 워커 설치
+// 서비스 워커 설치 및 기본 파일 캐싱
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(URLS_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-// 요청에 대한 응답 처리
+// 네트워크 우선 전략으로 요청 처리
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // 캐시에 응답이 있으면 그것을 반환, 없으면 네트워크로 요청
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => {
+      // 네트워크 요청이 실패하면 캐시에서 찾아 반환
+      return caches.match(event.request);
+    })
   );
 });
 
